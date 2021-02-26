d460afa39be12e1f0e4b9da760965c3dae917337
Bruna Ariele <bruna.arisoares@gmail.com>
2021-02-26 12:08:40 -0300

    Farma

---
+++ src/main/java/com/farmacia/farmacia/model/Produto.java
@@ -0,0 +1,76 @@
+package com.farmacia.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min= 5, max = 100)
+	private String nome;
+	
+	@NotNull
+	@Size(min= 5, max = 100)
+	private String marca;
+	
+	@NotNull
+	
+	private Double preco;
+	
+	
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categoria;
+	
+	
+	
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+
+}
